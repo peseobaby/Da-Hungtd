0d96fc642cde67a2d4d5a1b8cecfa2fa0aa20482
manhhd <mynameishoangmanh@gmail.com>
2019-05-19 14:55:26 +0700

    add layout

---
+++ resources/views/front/layout/head.blade.php
@@ -0,0 +1,15 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>
+    @yield('title')
+</title>
+@yield('before_styles')
+@stack('before_styles')
+<!--build:css css/styles.min.css -->
+<link rel="stylesheet" href="../assets/css/owl/owl.carousel.min.css">
+<link rel="stylesheet" href="../assets/css/ion.rangeSlider/css/ion.rangeSlider.css">
+<link rel="stylesheet" href="../assets/fonts/font-awesome/css/all.min.css">
+<link rel="stylesheet" href="../assets/css/ion.rangeSlider/css/ion.rangeSlider.skinFlat.css">
+<link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700&amp;subset=vietnamese" rel="stylesheet">
+@yield('after_styles')
+@stack('after_styles')
---
+++ resources/views/front/layout/layout-front.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    @include('front.layout.head')
+</head>
+<body>
+
+@yield('header')
+
+<!-- Main content -->
+@yield('content')
+<!-- /.content -->
+
+
+@yield('before_scripts')
+@stack('before_scripts')
+
+@yield('content')
+
+@yield('after_scripts')
+@stack('after_scripts')
+
+</body>
+</html>
