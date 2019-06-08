657873d9cafa01ddd4ef92671046d3a2f6eb8e56
Tran Hung <peseobaby4@gmail.com>
2019-06-08 17:42:39 +0700

    header

--- resources/views/front/layout/header-host.blade.php
+++ resources/views/front/layout/header-host.blade.php
@@ -1,4 +1,4 @@
-{{-- <header class="header--host">
+<header class="header--host">
     <div class="container host--nav">
         <div class="logo">
             <h1>Travel<span>trip</span></h1>
@@ -6,9 +6,9 @@
 
         <div class="nav__top">
             <ul>
-                <li><img src="{{ asset('assets/images/hanoi.png') }}" alt=""> {{ $user->name }} <i class="far fa-angle-down"></i>
+                <li><img src="{{ asset('assets/images/hanoi.png') }}" alt=""> {{ backpack_auth()->user()->name }} <i class="far fa-angle-down"></i>
                     <ul>
-                        <li><a href="{{ route('show.user', $user->id) }}">Profile</a></li>
+                        <li><a href="{{ route('show.user', backpack_auth()->user()->id ) }}">Profile</a></li>
                         <li><a href="">Logout</a></li>
                     </ul>
                 </li>
@@ -23,11 +23,10 @@
                 <li><a href="" class="active">Bảng thông tin</a></li>
                 <li><a href="{{route('front.hotel.search') }}">Tìm kiếm</a></li>
                 <li><a href="">Đặt phòng</a></li>
-                <li><a href="{{ route('front.hotel.show', $user->hotel->id) }}">khách sạn của bạn</a></li>
-                <li><a href="{{ route('show.user', $user->id) }}">Thông tin cá nhân</a></li>
-                <li><a href="{{ route('show.order', $user->hotel->id) }}">Danh sách order</a></li>
+                <li><a href="{{ route('front.hotel.show', backpack_user()->hotels->first()->id) }}">khách sạn của bạn</a></li>
+                <li><a href="{{ route('show.user', backpack_user()->id) }}">Thông tin cá nhân</a></li>
+                <li><a href="{{ route('show.order', backpack_user()->hotels->first()->id) }}">Danh sách đơn đặt phòng của khách sạn</a></li>
             </ul>
         </nav>
     </div>
 </header>
- --}}
