43f3672a85f580c56a0afb9927687a3ce0f3b3d5
Tran Hung <peseobaby4@gmail.com>
2019-06-09 11:45:00 +0700

    thanhtoan

--- app/Http/Controllers/Front/OrderController.php
+++ app/Http/Controllers/Front/OrderController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Order;
 use App\Models\User;
 use Carbon\Carbon;
+use App\Models\Room;
 
 class OrderController extends Controller
 {
@@ -40,7 +41,7 @@ class OrderController extends Controller
         return view('front.order.khachluutru', compact('orders', 'id'));
     }
 
-    public function setOrder(Request $request)
+    public function setOrder(Request $request, $id)
     {
         if (session()->has('REQUEST')) {
             $data = session()->get('REQUEST')[0];
@@ -50,7 +51,36 @@ class OrderController extends Controller
         }
         $data['create_at'] = date("Y-m-d H:i:s", strtotime($data['create_at']));
         $data['end_at'] = date("Y-m-d H:i:s", strtotime($data['end_at']));
-        $order = Order::create($data);
-        return view('front.order.thanhtoan');
+        $data['room_id'] = $id;
+        $now = Carbon::now()->toDateString();
+        $end_date = Carbon::parse($request->input('end_at'));
+        $create_date = Carbon::parse($request->input('create_at'));
+        $lengthOfEnd = $end_date->diffInDays($now);
+        $lengthOfCreate = $create_date->diffInDays($now);
+        $total = $lengthOfEnd - $lengthOfCreate + 1;
+        $price = Room::find($id)->price * $total;
+        $data['price'] = $price;
+        $order = Order::with('room')->create($data);
+        return view('front.order.thanhtoan', compact('order'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $data = [
+            'phone' => $request->phone,
+            'serial_number' => $request->serial_number,
+            'user_id' => backpack_user()->id,
+            'hotel_id' => backpack_user()->hotels->first()->id,
+        ];
+        Order::find($id)->update($data);
+        $order = Order::find($id);
+        return redirect()->route('my.order', $order->user_id)->with('alert', 'đặt phòng thành công');
+    }
+
+    public function showOrder($id)
+    {
+        $orders = Order::where('user_id', $id)->get();
+        $user = User::where('id', $id)->first();
+        return view('front.order.myorder', compact('orders', 'user'));
     }
 }
